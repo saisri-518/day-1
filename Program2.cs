8fe4f672a91cecdf31bcf4a5c9665765c8c464f6
saisri-518 <saisridonthula@gmail.com>
2022-01-31 16:56:58 +0530

    new

---
+++ Program2.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+class DemoSai
+{
+    public static void Main()
+    {
+        int a;
+            Console.WriteLine("Enter a number to check : ");
+               a=int.Parse(Console.ReadLine());
+        if(a <= 18)
+        {
+            Console.WriteLine("Minor");
+        }
+        else
+        {
+            Console.WriteLine("Majors");
+        }
+    }
+}
