ec48d57ef10ac718019e0656658fecca089caeee
saisri-518 <saisridonthula@gmail.com>
2022-02-01 10:15:23 +0530

    new

---
+++ Sum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum
+{
+    internal class Demosai
+    {
+        static void Main(string[] args)
+        {
+            int n1=int.Parse(Console.ReadLine());
+            int sum = 0;
+            for(int i=1;i<=n1;i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
