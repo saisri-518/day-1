32761b5b6dab798ed50b2aea63bdda1ce450dd37
saisri-518 <saisridonthula@gmail.com>
2022-02-01 10:13:22 +0530

    new

--- 3numbers.cs
+++ 3numbers.cs
@@ -4,21 +4,14 @@ class DemoSai
 {
     public static void Main()
     {
-        int a,b,c;
-            Console.WriteLine("Enter value to check : ");
-               a=int.Parse(Console.ReadLine());
-               b=int.Parse(Console.ReadLine());
-               c=int.Parse(Console.ReadLine());
-        if(a>>b && a>>C)
-        {
-            Console.WriteLine("a is greater");
-        }
-        else if(b>>c && b>>a)
-        {
-            Console.WriteLine("b is greater");
-        }
-        else{
-            Console.WriteLine("c is greater");
+        int n1=int.Parse(Console.ReadLine());
+            int sum = 0;
+            for(int i=1;i<=n1;i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
         }
     }
 }
