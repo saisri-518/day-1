1637cc8ac7669e33627facbb315a1d7a7108074f
saisri-518 <saisridonthula@gmail.com>
2022-02-02 10:09:47 +0530

    new

--- Repeat.cs
+++ Repeat.cs
@@ -1,23 +1,52 @@
 ﻿
-using System;
-class DemoSai
-{
-    static void Main(string[] args)
-        {
-            int[] arr = { 1, 2, 3, 5, 5, 5, 6 };
-            int count = 0;
-            for(int i = 0; i < arr.Length; i++)
+using System;  
+public class Demo 
+{  
+    public static void Main() 
+  {
+    int n,i,j=0,lrg,lrg2nd;
+  	int[] arr1 = new int[50];
+  
+         
+  
+       Console.Write("Input the size of array : ");
+	   n = Convert.ToInt32(Console.ReadLine()); 	   
+    /* Stored values into the array*/
+       Console.Write("Input {0} elements in the array :\n",n);
+       for(i=0;i<n;i++)
             {
-                for(int j=0; j < arr.Length; j++)
-                {
-                    if (arr[i] == arr[j])
-                    {
-                        count += 1;
-                    }
-                }
-                Console.WriteLine("{0} is repeated {1} times", arr[i], count);
-                count = 0;
-            }
-            Console.ReadLine();
+	      Console.Write("element - {0} : ",i);
+		  arr1[i] = Convert.ToInt32(Console.ReadLine()); 		  
+	    }
+/* find location of the largest element in the array */		
+   lrg=0;
+
+  for(i=0;i<n;i++)
+  {
+      if(lrg<arr1[i])
+	  {
+           lrg=arr1[i];
+           j = i;
+      }
+  }
+/* ignore the largest element and find the 2nd largest element in the array */		
+   lrg2nd=0;
+  for(i=0;i<n;i++)
+  {
+     if(i==j)
+        {
+          i++;  /* ignoring the largest element */
+          i--;
         }
+      else
+        {
+           if(lrg2nd<arr1[i])
+	         {
+               lrg2nd=arr1[i];
+             }
+        }
+  }
+
+  Console.Write("The Second largest element in the array is :  {0} \n\n", lrg2nd);
+}
 }
