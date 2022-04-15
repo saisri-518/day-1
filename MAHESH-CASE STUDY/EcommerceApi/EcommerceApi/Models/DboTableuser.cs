f37499365f6368fe56f8018332ad27ebc332b5a8
saisri-518 <saisridonthula@gmail.com>
2022-04-15 12:55:40 +0530

    casestudy

---
+++ MAHESH-CASE STUDY/EcommerceApi/EcommerceApi/Models/DboTableuser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EcommerceApi.Models
+{
+    public partial class DboTableuser
+    {
+        public int Userid { get; set; }
+        public string Username { get; set; }
+        public string Usermail { get; set; }
+        public string Password { get; set; }
+    }
+}
