25011775488dba99ba1880135f4b2846d60b3a80
saisri-518 <saisridonthula@gmail.com>
2022-04-22 10:14:53 +0530

    pro

---
+++ EcommerceWebApp/EcommerceWebApp/EcommerceWebApp/Controllers/RegisterController.cs
@@ -0,0 +1,48 @@
+﻿using EcommerceWebApp.Models;
+using EcommerceWebApp.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcommerceWebApp.Controllers
+{
+    public class RegisterController : Controller
+    {
+        EcommerceDbContext _ecommerceDbContext;
+        public RegisterController(EcommerceDbContext ecommerceDbContext)
+        {
+            _ecommerceDbContext = ecommerceDbContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SubmitData(Register register)
+        {
+            TblUserDetail tblUserDetail = new TblUserDetail();
+            tblUserDetail.FirstName = register.FirstName;
+            tblUserDetail.LastName = register.LastName;
+            tblUserDetail.Gender = register.Gender;
+            tblUserDetail.Email= register.Email;
+            tblUserDetail.CreatedOn = DateTime.Now;
+            _ecommerceDbContext.TblUserDetails.Add(tblUserDetail);
+            _ecommerceDbContext.SaveChanges();
+            TblUser tblUser = new TblUser();
+
+
+            tblUser.CreatedOn = tblUser.CreatedOn;
+            tblUser.IsDeleted = 0;
+            tblUser.UpdatedOn = DateTime.Now;
+            tblUser.UserName = register.Email;
+            tblUser.Password = register.FirstName;
+            _ecommerceDbContext.TblUsers.Add(tblUser);
+            _ecommerceDbContext.SaveChanges();
+            return RedirectToAction("Index");
+           
+
+
+        }
+    }
+}
---
+++ EcommerceWebApp/EcommerceWebApp/EcommerceWebApp/ViewModel/Register.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcommerceWebApp.ViewModel
+{
+    public class Register
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+    }
+}
